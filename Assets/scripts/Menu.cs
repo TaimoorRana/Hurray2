68a95a168f0b53468efbc357b01a152bc1b79503
aniis1987 <anis.feki.87@gmail.com>
2016-03-20 02:19:52 -0400

    version v1

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+    // Use this for initialization
+    public void Lecture()
+    {
+        Time.timeScale = 1.0f;
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0.0f;
+    }
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
