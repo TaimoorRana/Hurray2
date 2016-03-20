4ee273650eddfb972200a53db9c1444156f2c0c3
Taimoor <taimoorrana1@hotmail.com>
2016-03-19 22:30:46 -0400

    Merged Taimoor and Anis

---
+++ Assets/scripts/ControlRoad.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlRoad : MonoBehaviour {
+    public GameObject[] env=new GameObject[3];
+    public GameObject CameraMove;
+    float PosIntEnv;
+    int i=0;
+    int mul=1;
+    // Use this for initialization
+    void Start () {
+        PosIntEnv = CameraMove.transform.position.z;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Mathf.Abs(CameraMove.transform.position.z- PosIntEnv)  > 17.19742f*mul)
+        {
+            env[i].transform.position = new Vector3 (env[i].transform.position.x,env[i].transform.position.y,env[i].transform.position.z+ 17.19742f*3);
+            i++;
+            mul++;
+            if (i == 3)
+                i = 0;
+        }
+            
+    }
+}
---
+++ Assets/scripts/InstantiateObstacles.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateObstacles : MonoBehaviour {
+    public GameObject pref;
+    public GameObject reference;
+    public GameObject obstacles;
+    float temps = 0.0f;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float waitTime = Random.Range(5f,10.0f);
+        
+            if (temps>=waitTime)
+            {
+            (Instantiate(pref, new Vector3(Random.Range(-1.5F, 1.5F), 0.2f, reference.transform.position.z + Random.Range(5.0F, 10.0F)), Quaternion.identity) as  GameObject).transform.parent = this.transform; ;
+
+            temps = 0.0f;
+            }
+            else
+            temps = temps + Time.deltaTime;
+
+        //StartCoroutine(Example(waitTime));
+        //Instantiate(pref,new Vector3(Random.Range(-1.5F, 1.5F), 0.2f,reference.transform.position.z+ Random.Range(5.0F, 10.0F)), Quaternion.identity);
+	}
+    IEnumerator Example(float WT)
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(WT);
+        print(Time.time);
+    }
+}
