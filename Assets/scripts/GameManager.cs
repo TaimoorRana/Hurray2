37c826aa089ab36d82df2ecf7ab6e379434a8074
Taimoor <taimoorrana1@hotmail.com>
2016-03-20 12:16:32 -0400

    added gamemanager

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+	int points = 0;
+	public GameObject[] student;
+	public GameObject[] location;
+	public int studentsCount = 0;
+	// Use this for initialization
+	void Start () {
+		int locationIndex = 0;
+		int studentIndex = 0 ;
+		while(studentsCount != 0){
+			if(locationIndex >= location.Length)
+				locationIndex = 0;
+			if(studentIndex >= student.Length)
+				studentIndex = 0;
+			Instantiate(student[studentIndex++],location[locationIndex++].transform.position,Quaternion.identity);
+			points++;
+			studentsCount--;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void reducePoint(){
+		points--;
+	}
+
+	public int getPoints(){
+		return points;
+	}
+}
